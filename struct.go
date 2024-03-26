eecb775573e309f6d94378958c9818174e633c43
Shubham Katara <shubham.katara@trivago.com>
2024-03-26 12:54:20 +0100

    use pointers to change original struct value

--- struct.go
+++ struct.go
@@ -10,11 +10,15 @@ type user struct {
 	age       int
 }
 
-func (u user) userPrint() {
+func userPrint(u user) {
 	fmt.Println(u.age, u.lastName, u.firstName)
 }
 
-func user_main() {
+func clearName(u *user) {
+	u.firstName = ""
+}
+
+func userain() {
 	var u user
 	fmt.Println("Enter Username")
 	fmt.Scan(&u.firstName)
@@ -22,5 +26,8 @@ func user_main() {
 	fmt.Scan(&u.lastName)
 	fmt.Println("Enter Age")
 	fmt.Scan(&u.age)
-	u.userPrint()
+	userPrint(u)  // Here a new copy of u is created and is passed. If we want to work on changing the values of the variable u ( instead of creating a new value of it ), we need to pass a pointer instead of the variable
+	clearName(&u) // Here, as we wanted to clear the firstname that user has given and stored in struct u.firstname, we pass the pointer to that struct. This prevents go to create a new copy of struct u but instead work on the original one
+	userPrint(u)
+
 }
